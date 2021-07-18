7626bee38fb2efee4c2ee4ea9c4ea0e24c3162f5
mohamed <ragabmohame6@gmail.com>
2021-07-18 20:14:26 +0200

    Update server

--- src/server/index.js
+++ src/server/index.js
@@ -8,46 +8,42 @@ const fetch = require("node-fetch");
 const base_api_url = "https://api.meaningcloud.com/sentiment-2.1";
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require("cors");
 app.use(cors());
 
+app.use(express.json());
+
 app.use(express.static("dist"));
 
 const { TRUE } = require("node-sass");
-const corsOptions = {
-  origin: true,
-  maxAge: 3600,
-  optionsSuccessStatus: 200,
-  credentials: true,
-  origin: `${base_api_url}?key=${process.env.API_KEY}`,
-  methods: ["POST"],
-  allowedHeaders: ["Content-Type"],
-  preflightContinue: true,
-};
+
 console.log(__dirname);
 app.get("/", function (req, res) {
   // res.sendFile("dist/index.html");
   res.sendFile(path.resolve("src/client/views/index.html"));
 });
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-  console.log(" app listening on port 8081!");
-});
+
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
 
-app.options("/addURL", cors(corsOptions));
-app.post("/addURL", cors(corsOptions), async (req, res) => {
-  const wantedUrl = req.body.url;
-  wantedUrl ? console.log(`You entered: ${userInput}`) : console.log(`error`);
-  const url = `${base_api_url}?key=${process.env.API_KEY}&url=${wantedUrl}&lang=en`;
-  const fetchedData = await fetch(url);
-  const data = await fetchedData.json();
-  console.log(data);
-  res.send(data);
+app.post("/addURL",  (req, res) => {
+  try {
+    const wantedUrl = req.body.bodyData.url;
+    const url = `${base_api_url}?key=${process.env.API_KEY}&url=${wantedUrl}&lang=en`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(json => res.send(json));
+    
+  } catch(e) {
+    console.log(e)
+  }
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+  console.log(" app listening on port 8081!");
 });
